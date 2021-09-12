5e13eec16bca1648c67cd09a4eb6dca05d19340a
Vinicius Araujo <18546@unifcv.edu.br>
2021-09-12 11:29:52 -0300

    Initial commit

---
+++ Lavadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Adapter {
+    class Lavadora : Tomada, IAparelho {
+
+        public String Liga () {
+            this.Ligado = true;
+            Console.WriteLine ("A lavadora está lavando roupas");
+            this.Lavar ();
+            this.Centrifugar ();
+            this.Enxaguar ();
+            this.Centrifugar ();
+            this.Enxaguar ();
+            this.Centrifugar ();
+            return this.Desliga ();
+        }
+
+
+        public String Desliga () {
+            this.Ligado = false;
+            return "Agora a lavadora está desligada";
+        }
+        public void Lavar () {
+            Console.WriteLine ("\tAdicionando água e sabão em pó...");
+            Thread.Sleep (2000);
+            Console.WriteLine ("\tLavando...");
+        }
+        private void Enxaguar () {
+            Thread.Sleep (1000);
+            Console.WriteLine ("\tEnxaguando...");
+        }
+        private void Centrifugar () {
+            Thread.Sleep (1000);
+            Console.WriteLine ("\tCentrifugando...");
+        }
+
+    }
+}
---
+++ Tomada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter {
+    class Tomada {
+        private bool ligado;
+        public bool Ligado {
+            get {
+                return this.ligado;
+            }
+            set {
+                this.ligado = value;
+                Console.WriteLine (">>> {0}Ligando Aparelho", this.ligado ? "" : "des");
+            }
+        }
+    }
+}
